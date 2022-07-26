d644f2d2d2cd7bd7fefbbb04b9341767f6fc8cb6
Raph <duffscs@gmail.com>
2022-07-26 10:17:47 +0200

    cumulative time limit exeeced

--- cumulative/src/main.rs
+++ cumulative/src/main.rs
@@ -1,67 +1,68 @@
-use std::collections::HashMap;
-use std::time::Instant;
+use std::{
+    collections::{HashMap, HashSet},
+    io::{self, Read}, time::Instant,
+};
 
 fn main() {
-    // let mut input = String::new();
-    // stdin().read_to_string(&mut input).expect("Lecture de stdin");
-    // let mut entrer = input.lines().skip(1).map(|line| line.parse::<u64>().expect("i32")).collect::<Vec<u64>>();
-    // let mut max = *entrer.iter().max().unwrap();
-    // let mut hashset : HashSet<u64> = entrer.clone().into_iter().collect();
-    // let mut hashMap : HashMap<u64,u64> = HashMap::new();
-    // let mut a = 1;
-    // let mut sum = 1;
-    // hashMap.insert(0,0);
-    // hashMap.insert(1,1);
-    let mut a = Instant::now();
-    for i in 0..100000000 {
-        (i, sum_of_digits(i as u64));
-    }
-    println!("{:?}", a.elapsed().as_millis());
+    let mut input = String::new();
+    io::stdin()
+        .read_to_string(&mut input)
+        .expect("Lecture de stdin");
+    let mut word = input.split_whitespace();
+    let n = word.next().unwrap().parse::<usize>().unwrap();
+    let mut max = 0;
+    let set: HashSet<usize> = word
+        .clone()
+        .map(|e| {
+            let i = e.parse::<usize>().unwrap();
+            if i > max {
+                max = i;
+            }
+            return i;
+        })
+        .collect();
 
-    a = Instant::now();
-    let mut hash_map: HashMap<u64, u64> = HashMap::new();
-    for i in 0..100000000 {
-        (i, sod(i as u64, &mut hash_map));
+    let mut prev_ai = 1;
+    let mut prev_si = 1;
+    let mut map: HashMap<usize, usize> = HashMap::new();
+    let mut sod_vec: Vec<i64> = Vec::new();
+    sod_vec.push(0);
+    sod_vec.push(1);
+    for i in 2..=max {
+        sod_vec.push(sod(prev_ai));
+        prev_ai = (prev_ai + sod_vec.last().unwrap()) % ((10 as i64).pow(9) + 7);
+        prev_si = (prev_ai + prev_si) % ((10 as i64).pow(9) + 7);
+        if set.contains(&i) {
+            map.insert(i, prev_si as usize);
+        }
     }
-    println!("{:?}", a.elapsed().as_millis());
-    // for i in 2..=max {
-    //     a = a + sum_of_digits(a);
-    //     sum = (a + sum) % 1000000007;
-    //     if hashset.contains(&i) {
-    //         hashMap.insert(i,sum);
-    //     }
-    // }
-    // entrer.iter().for_each(|e| {
-    //     println!("{}",hashMap.get(e).unwrap());
-    // })
+
+    for _ in 0..n {
+        let i = &word.next().unwrap().parse::<usize>().unwrap();
+        print!("{} ", map.get(i).unwrap());
+    }
 }
 
-struct DigitIter(u64);
+fn sod(num: i64) -> i64 {
+    // let mut sum: i64 = 0;
+    // if num > 0 {
+    //     sum += num % 10;
+    //     sum += sod(num / 10);
+    // }
+    return DigitIter(num as usize).sum::<usize>() as i64;
+}
+
+struct DigitIter(usize);
 
 impl Iterator for DigitIter {
-    type Item = u64;
+    type Item = usize;
     fn next(&mut self) -> Option<Self::Item> {
         if self.0 == 0 {
-            return None;
+            None
+        } else {
+            let ret = self.0 % 10;
+            self.0 /= 10;
+            Some(ret)
         }
-        let ret = self.0 % 10;
-        self.0 = self.0 / 10;
-        Some(ret)
     }
-}
-
-fn sum_of_digits(entier: u64) -> u64 {
-    return DigitIter(entier).sum::<u64>();
-}
-
-fn sod(number: u64, map: &mut HashMap<u64, u64>) -> u64 {
-    if number == 0 {
-        return number;
-    }
-    if map.contains_key(&number) {
-        return *map.get(&number).unwrap();
-    }
-    let tmp = sod(number / 10, map);
-    map.insert(number, number % 10 + tmp);
-    return *map.get(&number).unwrap();
 }
